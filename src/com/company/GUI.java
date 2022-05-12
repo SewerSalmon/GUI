538a337b529994bbf2bb506a1b0c6c3f415a218d
SewerSalmon <bj.jwalker2004@gmail.com>
2022-05-12 10:15:01 +0100

    wqedffds

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GUI extends JPanel {
+
     public GUI() {
     }
 
@@ -13,6 +14,7 @@ public class GUI extends JPanel {
         this.setPreferredSize(new Dimension(width, height));
         button1 = new JButton(name);
         button1.setBounds(x, y, width, height);
+        button1.setLocation(new Point(x, y));
         button1.addActionListener(new ClickButton());
         add(button1);
     }
@@ -23,69 +25,76 @@ public class GUI extends JPanel {
             System.out.println(e.getActionCommand());
         }
     }
-        ImageIcon image;
-        int WIDTH;
-        int HEIGHT;
-        Point imageCorner;
-        Point prevPt=new Point(0,0);
-        public void DragPanel(String FileLocation) {
-            image = new ImageIcon(FileLocation);
-            WIDTH = image.getIconWidth();
-            HEIGHT = image.getIconHeight();
-            imageCorner = new Point(0, 0);
-            ClickListener clickListener = new ClickListener();
-            DragListener dragListener = new DragListener();
-            this.addMouseListener(clickListener);
-            this.addMouseMotionListener(dragListener);
+
+
+
+    ImageIcon image;
+    int WIDTH;
+    int HEIGHT;
+    Point imageCorner;
+    Point prevPt = new Point(0, 0);
+
+    public void DragPanel(String FileLocation) {
+        image = new ImageIcon(FileLocation);
+        WIDTH = image.getIconWidth();
+        HEIGHT = image.getIconHeight();
+        imageCorner = new Point(0, 0);
+        ClickListener clickListener = new ClickListener();
+        DragListener dragListener = new DragListener();
+        this.addMouseListener(clickListener);
+        this.addMouseMotionListener(dragListener);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (!(imageCorner == null)) {
+            image.paintIcon(this, g, (int) imageCorner.getX(), (int) imageCorner.getY());
         }
 
-        public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            try {
-                    image.paintIcon(this, g, (int) imageCorner.getX(), (int) imageCorner.getY());
-            }
-            catch(NullPointerException e) {
-                System.out.println("NullPointerException thrown!");
-            }
+    }
 
-        }
+    boolean ClickedInImage;
 
-        private class ClickListener extends MouseAdapter {
-            public void mousePressed(MouseEvent e) {
+    private class ClickListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            if ((e.getPoint().getX() <= imageCorner.getX() + WIDTH && e.getPoint().getX() >= imageCorner.getX()) && (e.getPoint().getY() <= imageCorner.getY() + HEIGHT && e.getPoint().getY() >= imageCorner.getY())) {
                 prevPt = e.getPoint();
+                ClickedInImage = true;
+            } else {
+                ClickedInImage = false;
             }
         }
+    }
 
-        private class DragListener extends MouseMotionAdapter {
+    private class DragListener extends MouseMotionAdapter {
 
-            public void mouseDragged(MouseEvent e) {
+        public void mouseDragged(MouseEvent e) {
+            if (ClickedInImage) {
                 Point currentPt = e.getPoint();//translate moves object by that much
                 imageCorner.translate((int) (currentPt.getX() - prevPt.getX()), (int) (currentPt.getY() - prevPt.getY()));
                 prevPt = currentPt;
                 repaint();
+
             }
         }
+    }
 
-
-    JRadioButton r1,r2;
-    public void radioButtonAdd (){
-            r1=new JRadioButton("Male");
-            r2=new JRadioButton("Female");
-            r1.setBounds(75,50,100,30);
-            r2.setBounds(75,100,100,30);
-            ButtonGroup bg = new ButtonGroup();
+    public void radioButtonAdd (String name,int x,int y,int width,int height, ButtonGroup e){
+            JRadioButton r1;
+            r1=new JRadioButton(name);
+            r1.setBounds(x,y,width,height);
+            r1.setLocation(new Point(x,y));
             r1.addActionListener(this::actionPerformedRadio);
-            r2.addActionListener(this::actionPerformedRadio);
-            bg.add(r1);bg.add(r2);
-            add(r1);add(r2);
+            e.add(r1);
+            add(r1);
         }
         public void actionPerformedRadio(ActionEvent e){
-        if(r1.isSelected()){
-            DragPanel("enhanced-buzz-1492-1379411828-15.jpg");
-            imageCorner.setLocation(prevPt);
-            repaint();
-        }
-        if(r2.isSelected()){
+            if(e.getActionCommand().equals("Male")){
+                DragPanel("enhanced-buzz-1492-1379411828-15.jpg");
+                imageCorner.setLocation(prevPt);
+                repaint();
+            }
+        if(e.getActionCommand().equals("Female")){
         DragPanel("image.jpg");
             imageCorner.setLocation(prevPt);
             repaint();
--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -1,5 +1,7 @@
 package com.company;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame{
 
@@ -7,14 +9,23 @@ public class MyFrame extends JFrame{
 
     MyFrame(){
         add(gui);
-        gui.buttonAdd("Isfan",200,100,1000,900);
+        gui.buttonAdd("Isfan",200,100,100,900);
         gui.buttonAdd("Giri",200,100,0,0);
-        gui.radioButtonAdd();
+
+        ButtonGroup bg = new ButtonGroup();
+        gui.radioButtonAdd("Male",100,20,100,200,bg);
+        gui.radioButtonAdd("Female",100,20,100,200,bg);
 
         this.setTitle("tets");
         this.setSize(1920, 1080);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
 
+    private class ClickButton implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e.getActionCommand());
+        }
     }
 }
